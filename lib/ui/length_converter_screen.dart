83a9184784309caa206f356a5b8126c3b1991a69
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-01-21 22:45:04 +0700

    Create design UI Length Converter Screen

--- lib/ui/length_converter_screen.dart
+++ lib/ui/length_converter_screen.dart
@@ -1,12 +1,13 @@
 /*
- * Created by Yudi Setiawan on 1/20/19 10:42 PM
+ * Created by Yudi Setiawan on 1/21/19 10:45 PM
  * Copyright (c) 2019. All right reserved.
- * Last modified 1/20/19 10:39 PM
+ * Last modified 1/21/19 10:43 PM
  */
 
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class LengthConverterScreen extends StatefulWidget {
   @override
@@ -32,6 +33,146 @@ class LengthConverterScreenState extends State<LengthConverterScreen> {
           onPressed: () {
             Navigator.pop(context);
           },
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Text("Hello"),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Text("World"),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: StaggeredGridView.count(
+                crossAxisCount: 4,
+                staggeredTiles: <StaggeredTile>[
+                  StaggeredTile.count(3, 1),
+                  StaggeredTile.count(1, 2),
+                  StaggeredTile.count(3, 1),
+                  StaggeredTile.count(3, 1),
+                  StaggeredTile.count(1, 2),
+                  StaggeredTile.count(2, 1),
+                  StaggeredTile.count(1, 1),
+                ],
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.grey[200],
+                        width: 0.5,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text("789"),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.grey[200],
+                        width: 0.5,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text("AC"),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.grey[200],
+                        width: 0.5,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text("456"),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.grey[200],
+                        width: 0.5,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text("123"),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.grey[200],
+                        width: 0.5,
+                      ),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.grey[200],
+                          width: 0.5,
+                        ),
+                      ),
+                      child: Center(
+                        child: Text("X"),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.grey[200],
+                        width: 0.5,
+                      ),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.grey[200],
+                          width: 0.5,
+                        ),
+                      ),
+                      child: Center(
+                        child: Text("0"),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.grey[200],
+                        width: 0.5,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text("."),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
